aabec1338abb2354b684d603e027e8be25cdff8b
Roger Lipscombe <roger@differentpla.net>
2012-06-26 20:42:14 +0100

    Hosting Nancy with WCF
    - Put together a simple Console Application.
    - Install-Package Nancy
    - Install-Package Nancy.Hosting.Wcf
    - Example code from https://github.com/NancyFx/Nancy/wiki/Hosting-nancy-with-wcf

---
+++ WcfHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Threading;
+using Nancy.Hosting.Wcf;
+
+namespace WcfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = new ManualResetEvent(false);
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine("^C");
+                stop.Set();
+            };
+
+            var host = new WebServiceHost(new NancyWcfGenericService(), new Uri("http://localhost:1234/base/"));
+            host.AddServiceEndpoint(typeof(NancyWcfGenericService), new WebHttpBinding(), "");
+            host.Open();
+
+            stop.WaitOne();
+
+            host.Close();
+        }
+    }
+}
